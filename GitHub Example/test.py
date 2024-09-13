d681e2f6575958071ad5877a080701633f37dabe
Fischer Wells <fisch.wells@gmail.com>
2024-09-12 20:54:12 -0600

    added sentiment function and git steps

--- GitHub Example/test.py
+++ GitHub Example/test.py
@@ -2,6 +2,10 @@ def veryimportant():
     print("Don't delete this very important function")
 
 
+def sentiment_analysis():
+    print("Doing sentiment analysis with deep learning")
+
+
 def main():
     print("This will be for our GitHub test")
     veryimportant()
