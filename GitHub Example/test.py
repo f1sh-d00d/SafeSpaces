443ac29f842f5a826bdad1455aab76c58a952244
Fischer Wells <fisch.wells@gmail.com>
2024-09-12 20:31:36 -0600

    added new function

--- GitHub Example/test.py
+++ GitHub Example/test.py
@@ -1,7 +1,10 @@
+def veryimportant():
+    print("Don't delete this very important function")
+
+
 def main():
     print("This will be for our GitHub test")
-    print("This change was made from another branch")
-
+    veryimportant()
 
 main()
 
